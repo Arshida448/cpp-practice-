4876e25abf925d71c516ec7891ece60db5539040
Arshida448 <akbrakbry666@gmail.com>
2026-08-18 19:30:21 +0330

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1;
+
+    cout << "please num1";
+    cin >> num1;
+
+    if (num1 > 0)
+        cout << "mosbet";
+    else
+        cout << "manfi";
+
+    return 0;
+}
